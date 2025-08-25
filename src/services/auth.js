63be39e16670dec4e758b532f4eb3ce6a08fb1fe
vktr-r2 <vktr.r2@protonmail.com>
2025-08-25 19:31:30 -0400

    Add auth service

---
+++ src/services/auth.js
@@ -0,0 +1,54 @@
+import api from './api';
+
+export const authService = {
+  login: async (email, password) => {
+    const response = await api.post('/users/sign_in', {
+      user: { email, password }
+    });
+    
+    const { user, token } = response.data;
+    localStorage.setItem('authToken', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    
+    return { user, token };
+  },
+
+  register: async (email, password, passwordConfirmation, name) => {
+    const response = await api.post('/users', {
+      user: {
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+        name
+      }
+    });
+    
+    const { user, token } = response.data;
+    localStorage.setItem('authToken', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    
+    return { user, token };
+  },
+
+  logout: async () => {
+    try {
+      await api.delete('/users/sign_out');
+    } finally {
+      localStorage.removeItem('authToken');
+      localStorage.removeItem('user');
+    }
+  },
+
+  getCurrentUser: () => {
+    const userStr = localStorage.getItem('user');
+    return userStr ? JSON.parse(userStr) : null;
+  },
+
+  getToken: () => {
+    return localStorage.getItem('authToken');
+  },
+
+  isAuthenticated: () => {
+    return !!localStorage.getItem('authToken');
+  }
+};
